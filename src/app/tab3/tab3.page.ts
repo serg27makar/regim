27487b090cd531b2a7c136576b9bc3ffbb7b5fbe
serg <serg27makar@gmail.com>
2019-07-11 20:57:52 +0300

    added camera

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { Camera } from '@ionic-native/camera';
+
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
 
-  constructor() {}
-
+  base64Image: string;
+  constructor(
+  ) {}
+  takePicture() {
+    Camera.getPicture({
+      destinationType: Camera.DestinationType.DATA_URL,
+      targetWidth: 1000,
+      targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+      this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+      console.log(err);
+    });
+  }
 }
